5a4b22cb1f35e2375283eb48bc0654966b4c1d47
Antti Keskinen <32198867+anttikes@users.noreply.github.com>
2022-12-25 01:15:37 +0200

    Function can now return values from Postgres

--- src/Products.API/Products.cs
+++ src/Products.API/Products.cs
@@ -1,10 +1,26 @@
-using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using Npgsql;
 
-namespace Merus.Power.Azure.Demo
+namespace Merus.Power.Demo
 {
+    internal class Product
+    {
+        public Guid Id { get; }
+        public string Name { get; }
+        public decimal Price { get; }
+        public int Quantity { get; }
+
+        internal Product(Guid id, string name, decimal price, int quantity)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Quantity = quantity;            
+        }
+    }
+
     public class Products
     {
         private readonly ILogger _logger;
@@ -14,19 +30,44 @@ namespace Merus.Power.Azure.Demo
             _logger = loggerFactory.CreateLogger<Products>();
         }
 
-        [Function("GetProducts")]
-        public HttpResponseData Run(
-            [HttpTrigger(AuthorizationLevel.Function, "GET", Route = "products")] HttpRequestData req
+        [Function("GetAllProducts")]
+        public async Task<HttpResponseData> RunAsync(
+            [HttpTrigger(AuthorizationLevel.Function, "GET", Route = "products")] HttpRequestData req,
+            CancellationToken cancellationToken
         )
         {
-            _logger.LogInformation("BEGIN: GET /api/products");
+            var connectionString = Environment.GetEnvironmentVariable("SQL_CONNECTION_STRING");
 
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new ArgumentNullException($"The environment variable SQL_CONNECTION_STRING is not defined");
+            }
 
-            _logger.LogInformation("END: GET /api/products");
+            await using var dataSource = NpgsqlDataSource.Create(connectionString);
 
-            return response;
+            var products = new List<Product>();
+
+            await using (var cmd = dataSource.CreateCommand("SELECT * FROM public.Products"))
+            await using (var reader = await cmd.ExecuteReaderAsync(cancellationToken))
+            {
+                while(await reader.ReadAsync(cancellationToken))
+                {
+                    var product = new Product(
+                        reader.GetFieldValue<Guid>(0),
+                        reader.GetFieldValue<string>(1),
+                        reader.GetFieldValue<decimal>(2),
+                        reader.GetFieldValue<int>(3)
+                    );
+
+                    products.Add(product);
+                }
+            }
+
+            var res = req.CreateResponse(System.Net.HttpStatusCode.OK);
+
+            await res.WriteAsJsonAsync(products);
+
+            return res;
         }
     }
 }
