af454c5ba38221c83c0295da3429161fc2455801
Antti Keskinen <32198867+anttikes@users.noreply.github.com>
2022-12-25 16:38:43 +0200

    Added basic OpenAPI stuff

--- src/Products.API/Products.cs
+++ src/Products.API/Products.cs
@@ -1,8 +1,14 @@
+using System.Net;
+using System.Web;
 using Merus.Power.Demo.Products.API.Data;
+using Merus.Power.Demo.Products.API.Models;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace Merus.Power.Demo.Products.API;
 
@@ -26,6 +32,9 @@ public sealed class Products
     /// <param name="req">An instance of <see cref="Microsoft.Azure.Functions.Worker.HttpTriggerAttribute" /> which defines the trigger properties and provides access to the http request</param>
     /// <param name="cancellationToken">An instance of <see cref="System.Threading.CancellationToken" /> which is triggered when host cancellation is requested</param>
     /// <returns>A collection of <see cref="Merus.Power.Demo.Products.API.Models.Product" /> instances</returns>    
+    [OpenApiOperation(operationId: "getProducts", tags: new[] { "products" }, Summary = "GetProducts", Description = "Retrieves all products", Visibility = OpenApiVisibilityType.Important)]
+    [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/json", bodyType: typeof(List<Product>), Summary = "Product data", Description = "All products")]
     [Function("GetProducts")]
     public async Task<HttpResponseData> RunAsync(
         [HttpTrigger(AuthorizationLevel.Function, "GET", Route = "products")] HttpRequestData req,
--- src/Products.API/Program.cs
+++ src/Products.API/Program.cs
@@ -1,10 +1,11 @@
 using Merus.Power.Demo.Products.API.Data;
+using Microsoft.Azure.Functions.Worker.Extensions.OpenApi.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 // Configures the generic .NET host when running inside a Container App
 var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureFunctionsWorkerDefaults(worker => worker.UseNewtonsoftJson())
     .ConfigureServices(services =>
     {
         services.AddDbContext<ProductContext>();
